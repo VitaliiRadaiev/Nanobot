9932c64052a94894545143089f8dd82833dbc2ac
Spacespire <aleksandra@spacespire.com>
2023-03-17 11:56:11 +0200

    fix

--- #src/common/hide-content/hide-content.js
+++ #src/common/hide-content/hide-content.js
@@ -17,12 +17,20 @@
 
                     if(hideContentEl.classList.contains('hide-content-is-open')) {
                         hideContentEl.classList.remove('hide-content-is-open')
-                        this.utils.slideUp(hideContentEl);
+                        //hideContentEl.style.height = hideContentEl.scrollHeight + 'px';
+                        hideContentEl.style.height = '33rem';
+                        setTimeout(() => {
+                            hideContentEl.removeAttribute('style');
+                        },500)
                         btn.innerText = btnTextCloseState;
                         btn.classList.remove('hide-content-is-open');
                     } else {
                         hideContentEl.classList.add('hide-content-is-open')
-                        this.utils.slideDown(hideContentEl);
+                        console.log(hideContentEl.scrollHeight);
+                        hideContentEl.style.height = hideContentEl.scrollHeight + 'px';
+                        // setTimeout(() => {
+                        //     hideContentEl.style.height = 'auto';
+                        // },500)
                         btn.innerText = btnTextOpenState;
                         btn.classList.add('hide-content-is-open');
                     }
