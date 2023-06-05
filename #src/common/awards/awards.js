d43954f745f2f434864d56761f68027128f93691
Spacespire <aleksandra@spacespire.com>
2023-06-05 14:44:56 +0300

    awards slider updated

--- #src/common/awards/awards.js
+++ #src/common/awards/awards.js
@@ -1,7 +1,9 @@
 {
     let awards = document.querySelector('[data-slider="awards"]');
     if(awards) {
-        let shadowBtnRight = awards.querySelector('.awards__shadow-right');
+        let btnLeft = awards.querySelector('.awards__shadow-left');
+        let btnRight = awards.querySelector('.awards__shadow-right');
+
         let sliderData = new Swiper(awards.querySelector('.swiper'), {
             speed: 600,
             breakpoints: {
@@ -18,10 +20,52 @@
             }
         });
 
-        if(shadowBtnRight) {
-            shadowBtnRight.addEventListener('mousemove', () => {
-                awards.classList.add('awards--init')
-            })
+        setTimeout(() => {
+            sliderData.update();
+        },1000)
+        function setButtonsVisibility(sliderData) {
+            if(sliderData.isBeginning) {
+                btnLeft.classList.add('hide');
+            } else {
+                btnLeft.classList.remove('hide');
+            }
+            if(sliderData.isEnd) {
+                btnRight.classList.add('hide');
+            } else {
+                btnRight.classList.remove('hide');
+            }
         }
+
+        setButtonsVisibility(sliderData);
+
+        sliderData.on('afterInit', () => {
+            setButtonsVisibility(sliderData);
+        })
+        sliderData.on('transitionStart', (sliderData) => {
+            setButtonsVisibility(sliderData);
+        })
+
+
+        let idBtnRight = null;
+        btnRight.addEventListener('mouseenter', () => {
+            sliderData.slideNext();
+            idBtnRight = setInterval(() => {
+                sliderData.slideNext();
+            },1000)
+        })
+        btnRight.addEventListener('mouseleave', () => {
+            clearInterval(idBtnRight);
+        })
+
+        let idBtnLeft = null;
+        btnLeft.addEventListener('mouseenter', () => {
+            sliderData.slidePrev();
+            idBtnLeft = setInterval(() => {
+                sliderData.slidePrev();
+            },1000)
+        })
+        btnLeft.addEventListener('mouseleave', () => {
+            clearInterval(idBtnLeft);
+        })
     }
 }
--- dist/js/async.js
+++ dist/js/async.js
@@ -212,7 +212,9 @@ window.popup = {
 		{
     let awards = document.querySelector('[data-slider="awards"]');
     if(awards) {
-        let shadowBtnRight = awards.querySelector('.awards__shadow-right');
+        let btnLeft = awards.querySelector('.awards__shadow-left');
+        let btnRight = awards.querySelector('.awards__shadow-right');
+
         let sliderData = new Swiper(awards.querySelector('.swiper'), {
             speed: 600,
             breakpoints: {
@@ -229,11 +231,53 @@ window.popup = {
             }
         });
 
-        if(shadowBtnRight) {
-            shadowBtnRight.addEventListener('mousemove', () => {
-                awards.classList.add('awards--init')
-            })
+        setTimeout(() => {
+            sliderData.update();
+        },1000)
+        function setButtonsVisibility(sliderData) {
+            if(sliderData.isBeginning) {
+                btnLeft.classList.add('hide');
+            } else {
+                btnLeft.classList.remove('hide');
+            }
+            if(sliderData.isEnd) {
+                btnRight.classList.add('hide');
+            } else {
+                btnRight.classList.remove('hide');
+            }
         }
+
+        setButtonsVisibility(sliderData);
+
+        sliderData.on('afterInit', () => {
+            setButtonsVisibility(sliderData);
+        })
+        sliderData.on('transitionStart', (sliderData) => {
+            setButtonsVisibility(sliderData);
+        })
+
+
+        let idBtnRight = null;
+        btnRight.addEventListener('mouseenter', () => {
+            sliderData.slideNext();
+            idBtnRight = setInterval(() => {
+                sliderData.slideNext();
+            },1000)
+        })
+        btnRight.addEventListener('mouseleave', () => {
+            clearInterval(idBtnRight);
+        })
+
+        let idBtnLeft = null;
+        btnLeft.addEventListener('mouseenter', () => {
+            sliderData.slidePrev();
+            idBtnLeft = setInterval(() => {
+                sliderData.slidePrev();
+            },1000)
+        })
+        btnLeft.addEventListener('mouseleave', () => {
+            clearInterval(idBtnLeft);
+        })
     }
 }
 		{
